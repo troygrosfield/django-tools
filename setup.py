bf22ca4cb0d5e01d096a22faae478c571843c56d
John Vandenberg <jayvdb@gmail.com>
2019-03-10 21:58:44 +0700

    MANIFEST.in: Add tests and docs to sdist
    Also add exclusion in setup() to prevent tests in sdist
    from being an installed package.
    
    Closes https://github.com/InfoAgeTech/django-core/issues/4

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     maintainer='Troy Grosfield',
     url='https://github.com/infoagetech/django-core',
     license='MIT',
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests*']),
     include_package_data=True,
     zip_safe=False,
     setup_requires=[
